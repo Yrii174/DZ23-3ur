9a46fc2963c6373562579e6fe19617ece7283a31
Yrii <yran74@mail.ru>
2022-10-19 05:49:20 +0300

    задача 23

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿System.Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int d = 0;
+int bbb = 0;
+if (a>0)
+{
+int b = 1;
+    while (b<=a) 
+    {
+       bbb = b*b*b;
+       System.Console.Write(bbb+" ");
+       b++;
+    }
+}
+else
+{
+int b = -1;
+    while (b>=a) 
+    {
+       bbb = b*b*b;
+       System.Console.Write(bbb+" ");
+       b--;
+    }
+}
+       
